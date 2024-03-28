dbf05455cea4b44ca31484ed1d7e9091563446c5
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-03-28 01:32:35 -0400

    Add files via upload
    added lab7

---
+++ Lab07_101369421/exercise-2/my-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { InputFormatDirective } from './input-format.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    InputFormatDirective
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
